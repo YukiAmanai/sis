61650d3d56c428118b94d2d046e29f80ef26e6d5
AmanaiYuki <yuyu15152525@outlook.jp>
2020-11-30 20:15:29 +0900

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models; 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        return view('users.show', ['user'=>$user]);
+    }
+
+    public function edit()
+    {
+        $user = Auth::user();
+
+        return view('users.edit', ['user' => $user]);
+    }
+
+    public function update()
+    {
+        dd('動いた！');
+    }
+}
