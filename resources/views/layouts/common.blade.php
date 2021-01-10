7d78f840ff1267d21ee4520e2c1ee871bef046e2
AmanaiYuki <yuyu15152525@outlook.jp>
2021-01-10 17:58:08 +0900

    add top page

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -6,15 +6,22 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>SIS</title>
         <!-- cssをインポート -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+        <script src="js/top.js"></script>
         <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+    
     </head>
     <body>
         @include('parts.header')
-        
+       
         @yield('content')
-            <footer class="footer bg-dark  fixed-bottom">
-                @include('parts.footer')
-            </footer>
+        <footer class="footer bg-dark  fixed-bottom">
+            @include('parts.footer')          
+        </footer>
         <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
--- resources/views/parts/image.blade.php
+++ resources/views/parts/image.blade.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<ul class="slider">
+    <li><img src="001.JPG" alt="image01"></li>
+    <li><img src="002.jpg" alt="image02"></li>
+    <li><img src="003.jpg" alt="image03"></li>
+    <li><img src="004.jpg" alt="image04"></li>
+</ul>
 
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="slick.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="slick-theme.css" media="screen" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="slick.min.js"></script>
-    <title>SIS</title>
-</head>
-<body>
-<div class="viewer">
-    <ul>
-        <li><img src="img/photo01.jpg" width="400" height="300" alt=""></li>
-        <li><img src="img/photo02.jpg" width="400" height="300" alt=""></li>
-        <li><img src="img/photo03.jpg" width="400" height="300" alt=""></li>
-        <li><img src="img/photo04.jpg" width="400" height="300" alt=""></li>
-        <li><img src="img/photo05.jpg" width="400" height="300" alt=""></li>
-    </ul>
-</div><!--/.viewer-->
-
-</body>
-</html>
