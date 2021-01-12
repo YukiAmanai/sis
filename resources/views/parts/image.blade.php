f23216c4ba0395bcb648025db70794ef52c9cf4c
AmanaiYuki <yuyu15152525@outlook.jp>
2021-01-12 21:57:18 +0900

    add top fixed

--- resources/views/parts/image.blade.php
+++ resources/views/parts/image.blade.php
@@ -1,7 +1,7 @@
 <ul class="slider">
-    <li><img src="images/img01.jpg" alt=""></li>
-    <li><img src="images/img02.jpg" alt=""></li>
     <li><img src="images/img.03.jpg" alt=""></li>
     <li><img src="images/img.04.jpg" alt=""></li>
+    <li><img src="images/img.05.jpg" alt=""></li>
+    <li><img src="images/img.06.jpg" alt=""></li>
 </ul>
 
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,17 +2,19 @@
 
 @section('content')
 
+        <h2>シューズの情報を投稿しょう！</h2>
+
 @include('parts.image')
 
 <script src="js/top.js"></script>
-        <h2>シューズの情報を投稿しょう！</h2>
 
-        <h3>ネット通販で靴を気軽に買えるようにする</h3>
+<a href="{{ route('categories.index') }}">START</a>
+
+<h3>ネット通販で靴を気軽に買えるようにする</h3>
             <p>皆さんネットで靴を購入されたことはありますか？<br>
             今ネット通販で洋服や靴を購入するのが主流になっています<br>
             しかし靴に関してはサイズ感が分からなかったり、購入後の見た目のイメージが異なったりして購入するのが心配だったりしますよね？<br>
             その問題解消するために靴の情報を投稿し、第三者同士で情報共有できるSNSを作りました！<br>
             </p>
-        <a href= "{{route('categories.index') }}">START</a>
 
 @endsection
