96dee166bbba9929c1a8433d95b89fbd073b2b6c
AmanaiYuki <yuyu15152525@outlook.jp>
2021-01-29 18:29:35 +0900

    add header

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -32,6 +32,7 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('categories.index') }}">カテゴリー一覧</a>
                                     <a class="dropdown-item" href="{{ route('bookmarks.index') }}">ブックマーク</a>
+                                    <a class="dropdown-item" href="{{ route('users.update') }}">ユーザー情報変更</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
