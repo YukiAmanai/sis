211a5d17d89fbcde9fd62734cd550430134e351b
AmanaiYuki <yuyu15152525@outlook.jp>
2021-02-06 08:33:03 +0900

    fix css

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,7 +3,12 @@
 @section('content')
 <form method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
  @csrf
- <input id="image" type="file" name="image"> 
+ <div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+      <div class="card-header">投稿する</div>
+      <input id="image" type="file" name="image"> 
        <div class="form">
            <div class="form-title">
              <label for="title">シューズ名</label> 
@@ -11,14 +16,21 @@
            </div>
    
            <div class="form-body">
-             <label for="body" class="form-content">内容</label> 
+             <label for="body" class="col-md-4 col-form-label text-md-right">内容</label> 
              <textarea id="body" class="" name="body" cols="50" rows="10"></textarea> 
                 <input method="GET" type="hidden" name="category_id" value="{{ $category_id }}">       
            </div>
-           
-           <div class="form-submit">
-             <button type="submit">投稿する</button>
+           <div class="form-group row mb-0">
+              <div class="col-md-8 offset-md-4">
+             <button class="btn btn-primary" type="submit">投稿する</button>
            </div>
        </div>
+       </div>
+      </div>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
 </form>
 @endsection
