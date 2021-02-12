73ddeba57f213851ab53e88cb3505563f7891d2d
AmanaiYuki <yuyu15152525@outlook.jp>
2021-02-12 20:25:32 +0900

    validation Post,Reply

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -16,17 +16,17 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-      <div class="card-header">投稿する</div>
-      <input id="image" type="file" class="form-control-file" name="image"> 
+      <div class="card-header">投稿する</div> 
        <div class="form">
+       <input id="image" type="file" class="form-control-file" name="image">
            <div class="form-title">
              <label for="title">シューズ名</label> 
              <input id="title"class="" name="title" value="">
            </div>
    
            <div class="form-body">
-             <label for="body" class="col-md-4 col-form-label text-md-right">内容</label> 
-             <textarea id="body" class="" name="body" cols="50" rows="10"></textarea> 
+             <label for="body" class="col-md-4 col-form-label">内容</label> 
+             <textarea class="form-control" id="body" name="body"cols="50" rows="10"></textarea> 
                 <input method="GET" type="hidden" name="category_id" value="{{ $category_id }}">       
            </div>
            <div class="form-group row mb-0">
