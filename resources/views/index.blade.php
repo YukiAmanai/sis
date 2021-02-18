1adfca39e26d615b673d22e6ece18b866616b95d
AmanaiYuki <yuyu15152525@outlook.jp>
2021-02-18 10:43:23 +0900

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
   <form class="form-inline my-2 my-lg-0 ml-2">
         <div class="form-group">
         <input method="GET" type="hidden" name="category_id" value="{{ $category_id }}">
-        <input type="search" class="form-control mr-sm-2" name="title" value="{{$title}}" placeholder="キーワードを入力" aria-label="検索...">
+        <input type="search" class="form-control mr-sm-2" name="title" value="{{$title}}" placeholder="シューズ名を入力" aria-label="検索...">
         </div>
         <input type="submit" value="検索" class="btn btn-info">
   </form>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,10 @@
     <div class="card">
       <div class="card-header">{{ $post->user->name }}</div>
       <div class="card-body">
-        <p class="card-text">{{ $post->body }}</p>
+        <p class="card-text">{!! nl2br(e($post->body)) !!}</p>
+        <div>
+          <img src="data:image/png;base64,{{ $post->image }}" alt="image" style="width: 30%; height: auto;">
+        </div>
     @auth
      @unless($bookmarked)
       <form method="POST" action="{{ route('bookmarks.add', $post->id) }}">
